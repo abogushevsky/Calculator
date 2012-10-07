e207beebda560e63d7b1c454835a2f290ce5eb7f
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2012-10-07 15:40:11 +0400

    Graph view controller added

---
+++ Calculator/CalculatorGraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorGraphViewController.h
+//  Calculator
+//
+//  Created by Anton Bogushevsky on 07.10.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorGraphViewController : UIViewController
+
+@end
---
+++ Calculator/CalculatorGraphViewController.m
@@ -0,0 +1,38 @@
+//
+//  CalculatorGraphViewController.m
+//  Calculator
+//
+//  Created by Anton Bogushevsky on 07.10.12.
+//
+//
+
+#import "CalculatorGraphViewController.h"
+
+@interface CalculatorGraphViewController ()
+
+@end
+
+@implementation CalculatorGraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
